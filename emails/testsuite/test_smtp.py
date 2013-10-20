f0479205d1681bacda29629e5cfd84aef64d131f
Sergey Lavrinenko <s@lavr.me>
2013-10-20 17:29:49 +0400

    minor tests updates

--- emails/testsuite/test_smtp.py
+++ emails/testsuite/test_smtp.py
@@ -15,11 +15,10 @@ def test_factory():
     server_params = { 'host': 'invalid-server.invalid-domain.xxx', 'port': 25 }
     server1 = factory[server_params]
     assert isinstance(server1, SMTPBackend)
-
     server2 = factory[server_params]
     assert server1==server2
 
-def test_exceptions():
+def test_exception_1():
 
     # IOError: [Errno 8] nodename nor servname provided, or not known
     server_params = { 'host': 'invalid-server.invalid-domain.xxx', 'port': 25 }
@@ -34,8 +33,10 @@ def test_exceptions():
         assert response.error.errno==8
 
 
+def test_exception_2(smtp_server):
     # Server disconnected
-    server_params = { 'host': 'alt1.aspmx.l.google.com', 'port': 25, 'debug':1 }
+    host, port = smtp_server
+    server_params = { 'host': host, 'port': port, 'debug':1 }
 
     message_params = {'html':'<p>Test from python-emails',
                       'mail_from': 's@lavr.me',
@@ -45,16 +46,24 @@ def test_exceptions():
                        'from_addr': 's@lavr.me',
                        'msg': emails.html(**message_params).as_string()}
 
-    if HAS_INTERNET_CONNECTION:
-        server = SMTPBackend(**server_params)
-        server.open()
-        server.connection.sock.close()  # simulate disconnect
-        response = server.sendmail(**sendmail_params)
-        print(response)
 
+    server = SMTPBackend(**server_params)
+    server.open()
+    server.connection.sock.close()  # simulate disconnect
+    response = server.sendmail(**sendmail_params)
+    print(response)
+
+
+def test_send_via_django_backend(django_email_backend):
+
+    message_params = {'html':'<p>Test from python-emails',
+                      'mail_from': 's@lavr.me',
+                      'mail_to': 'sergei-nko@yandex.ru',
+                      'subject': 'Test from python-emails'}
+    msg = emails.html(**message_params)
 
 if __name__=="__main__":
     import logging
     logging.basicConfig(level=logging.DEBUG)
     test_factory()
-    test_exceptions()
+    test_exception_1()
