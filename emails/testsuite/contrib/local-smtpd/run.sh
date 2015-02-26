7c640f3919dbcb3106b5bfda7ee702378c05a0e0
Sergey Lavrinenko <s@lavr.me>
2015-02-26 14:26:40 +0300

    smtp test run script fixed

--- emails/testsuite/contrib/local-smtpd/run.sh
+++ emails/testsuite/contrib/local-smtpd/run.sh
@@ -2,8 +2,13 @@
 # Looks like python3 has a bug in smtpd (SMTPSenderRefused: (503, 'Error: send HELO first'...)
 # So we try to start python2
 
-PYTHON=python2.7
-# next line doesn't works actually. TODO: fix it
-(which python2.7 && export PYTHON=python2.7) || (which python2.6 && export PYTHON=python2.6) ||  export PYTHON=python
+if which python2.7; then
+    PYTHON=python2.7
+elif which python2.6; then
+    PYTHON=python2.6
+else
+    PYTHON=python
+fi
+
 echo "$PYTHON run.py $@"
 $PYTHON run.py $@
