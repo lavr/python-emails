048cd77411db78bec774a8ec1d0be1f8b8efd56f
Sergey Lavrinenko <s@lavr.me>
2017-01-17 18:43:11 +0300

    release 0.5.11

--- emails/__init__.py
+++ emails/__init__.py
@@ -38,10 +38,10 @@ Links
 from __future__ import unicode_literals
 
 __title__ = 'emails'
-__version__ = '0.5.10'
+__version__ = '0.5.11'
 __author__ = 'Sergey Lavrinenko'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2013-2016 Sergey Lavrinenko'
+__copyright__ = 'Copyright 2013-2017 Sergey Lavrinenko'
 
 USER_AGENT = 'python-emails/%s' % __version__
 
