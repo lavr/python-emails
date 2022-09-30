eb1adb459116bcc66b7e413a468e1a97336034d9
Sergey Lavrinenko <s@lavr.me>
2022-09-30 13:30:56 +0300

    Change pipy classifiers list. Fixes #165

--- setup.py
+++ setup.py
@@ -134,7 +134,6 @@ settings.update(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: BSD License',
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
