43a3f8fcedd4d6b3449d66a6fad556fb471133d9
Sergey Lavrinenko <s.lavrinenko@gmail.com>
2026-03-31 17:44:08 +0300

    Add mypy type checking to CI
    Configure mypy in setup.cfg with per-module overrides:
    - emails.message: suppress mixin pattern false positives
    - emails.packages: ignore vendored DKIM code
    - emails.backend.smtp.client: private smtplib attrs
    - emails.template, emails.django: optional deps
    
    Add targeted type: ignore comments for known safe patterns.
    Add tox typecheck environment and CI job.
    
    mypy now passes clean on 26 source files.

--- emails/signers.py
+++ emails/signers.py
@@ -30,7 +30,7 @@ class DKIMSigner:
 
         # Compile private key
         try:
-            privkey = parse_pem_private_key(to_bytes(privkey))
+            privkey = parse_pem_private_key(to_bytes(privkey))  # type: ignore[arg-type]
         except UnparsableKeyError as exc:
             raise DKIMException(exc)
 
@@ -43,12 +43,13 @@ class DKIMSigner:
             # pydkim module parses message and privkey on each signing
             # this is not optimal for mass operations
             # TODO: patch pydkim or use another signing module
-            return dkim.sign(message=message, **self._sign_params)
+            return dkim.sign(message=message, **self._sign_params)  # type: ignore[no-any-return]
         except DKIMException:
             if self.ignore_sign_errors:
                 logging.exception('Error signing message')
             else:
                 raise
+        return None
 
     def get_sign_bytes(self, message: bytes) -> bytes | None:
         return self.get_sign_string(message)
@@ -57,10 +58,11 @@ class DKIMSigner:
         # pydkim returns string, so we should split
         s = self.get_sign_string(message)
         if s:
-            (header, value) = to_native(s).split(': ', 1)
+            (header, value) = to_native(s).split(': ', 1)  # type: ignore[union-attr]
             if value.endswith("\r\n"):
                 value = value[:-2]
             return header, value
+        return None
 
     def sign_message(self, msg: MIMEMultipart) -> MIMEMultipart:
         """
@@ -71,9 +73,9 @@ class DKIMSigner:
         # but py3 smtplib requires str to send DATA command (#
         # so we have to convert msg.as_string
 
-        dkim_header = self.get_sign_header(to_bytes(msg.as_string()))
+        dkim_header = self.get_sign_header(to_bytes(msg.as_string()))  # type: ignore[arg-type]
         if dkim_header:
-            msg._headers.insert(0, dkim_header)
+            msg._headers.insert(0, dkim_header)  # type: ignore[attr-defined]
         return msg
 
     def sign_message_string(self, message_string: str) -> str:
@@ -85,12 +87,12 @@ class DKIMSigner:
         # but py3 smtplib requires str to send DATA command
         # so we have to convert message_string
 
-        s = self.get_sign_string(to_bytes(message_string))
-        return s and to_native(s) + message_string or message_string
+        s = self.get_sign_string(to_bytes(message_string))  # type: ignore[arg-type]
+        return s and to_native(s) + message_string or message_string  # type: ignore[operator]
 
     def sign_message_bytes(self, message_bytes: bytes) -> bytes:
         """
         Insert DKIM header to message bytes
         """
         s = self.get_sign_bytes(message_bytes)
-        return s and to_bytes(s) + message_bytes or message_bytes
+        return s and to_bytes(s) + message_bytes or message_bytes  # type: ignore[operator]
