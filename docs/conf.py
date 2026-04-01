3a38cab10fad8996376ca74af8e2866e5f1419a0
Sergey Lavrinenko <s@lavr.me>
2026-04-01 11:26:10 +0300

    Update docs config (#208)
    * Update docs config: auto-detect version, remove Travis button
    
    - Read version/release from emails.__version__ instead of hardcoded 0.5
    - Remove travis_button option from theme configs (Travis CI is no longer used)
    - Fix intersphinx_mapping format for modern Sphinx
    
    * Use docs/requirements.txt for documentation build dependencies in CI
    
    * Add missing docs/_static directory

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,6 @@
 import sys, os
 sys.path.append(os.path.abspath('_themes'))
+sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('.'))
 
 from conf_base import *
@@ -7,6 +8,7 @@ from conf_theme_alabaster import *
 
 #from conf_theme_flask import *
 
-version = '0.5'
-release = '0.5.1'
+from emails import __version__ as _emails_version
+version = '.'.join(_emails_version.split('.')[:2])
+release = _emails_version
 html_theme_path = ['_themes', ] + html_theme_path
